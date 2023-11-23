4f45b645e6b07757accf5dd2a161eca0487da023
kuku20 <lluu3790@gmail.com>
2023-11-23 10:34:03 -0600

    canonical and ad

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ActivationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private readonly router: Router){}
+  ngOnInit(): void {
+    this.seo()
+  }
   title = 'pdftools';
+
+  seo(){
+    this.router.events.pipe(filter(event =>event instanceof ActivationEnd)).subscribe((event) => {
+      const canonical = document.querySelector('link[rel="canonical"]') as HTMLAnchorElement | null;
+      if (canonical !== null) {
+        canonical.href = window.location.origin + window.location.pathname;
+      }
+  });
+  }
 }
