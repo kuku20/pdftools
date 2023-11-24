f3f911fe4a0882a885b08c186b18bc23941d71fc
kuku20 <lluu3790@gmail.com>
2023-11-23 22:07:13 -0600

    adsend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AdsenseModule } from 'ng2-adsense';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MergeSplitComponent } from './merge-split/merge-split.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {DragDropModule} from '@angular/cdk/drag-drop';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 import { SplitComponent } from './split/split.component';
 @NgModule({
-  declarations: [
-    AppComponent,
-    MergeSplitComponent,
-    SplitComponent
-  ],
+  declarations: [AppComponent, MergeSplitComponent, SplitComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    DragDropModule
+    DragDropModule,
+    AdsenseModule.forRoot({
+      adClient: 'ca-pub-7470471186753851',
+      adSlot: 7408304468,
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
